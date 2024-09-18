59ecaa6ba086e256180f6ca009f3a31c02dd7fb3
Douglas-Lee <leeys.top@gmail.com>
2024-09-18 23:30:56 +0800

    fix test case

--- test/admin/endpoints_test.go
+++ test/admin/endpoints_test.go
@@ -87,7 +87,7 @@ var _ = Describe("/endpoints", Ordered, func() {
 				assert.Nil(GinkgoT(), err)
 				assert.Equal(GinkgoT(), 400, resp.StatusCode())
 				assert.Equal(GinkgoT(),
-					`{"message":"Request Validation","error":{"message":"request validation","fields":{"method":"invalid method","url":"invalid url"}}}`,
+					`{"message":"Request Validation","error":{"message":"request validation","fields":{"method":"required field missing","url":"required field missing"}}}`,
 					string(resp.Body()))
 			})
 		})

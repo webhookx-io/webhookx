a9cbe95b07d241d35bd08763992ba2d9a1b51343
webhookx-x <webhookx@gmail.com>
2024-09-24 23:03:20 +0800

    fix(migrations): add default value to avoid migration fails.

--- db/migrations/2_attempts.up.sql
+++ db/migrations/2_attempts.up.sql
@@ -1,5 +1,4 @@
-ALTER TABLE IF EXISTS ONLY "attempts" ADD COLUMN IF NOT EXISTS "trigger_mode" VARCHAR(10) NOT NULL;
+ALTER TABLE IF EXISTS ONLY "attempts" ADD COLUMN IF NOT EXISTS "trigger_mode" VARCHAR(10) NOT NULL DEFAULT 'INITIAL';
 ALTER TABLE IF EXISTS ONLY "attempts" ADD COLUMN IF NOT EXISTS "exhausted" BOOLEAN NOT NULL DEFAULT false;
 
-UPDATE "attempts" SET trigger_mode = 'INITIAL' where attempt_number = 1;
 UPDATE "attempts" SET trigger_mode = 'AUTOMATIC' where attempt_number != 1;

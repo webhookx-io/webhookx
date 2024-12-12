f99e592daa45b54c8c618840d1a2448e38ab290f
Douglas-Lee <leeys.top@gmail.com>
2024-12-13 01:50:58 +0800

    delete unused code

--- cmd/utils.go
+++ cmd/utils.go
@@ -2,10 +2,7 @@ package cmd
 
 import (
 	"fmt"
-	"io"
-	"net/http"
 	"strings"
-	"time"
 )
 
 var ANSWERS = map[string]bool{
@@ -20,26 +17,4 @@ func prompt(q string) bool {
 	var answer string
 	_, _ = fmt.Scan(&answer)
 	return ANSWERS[strings.ToLower(answer)]
-}
-
-func sendHTTPRequest(req *http.Request, timeout time.Duration) (string, error) {
-	client := http.Client{
-		Timeout: timeout,
-	}
-	resp, err := client.Do(req)
-	if err != nil {
-		return "", err
-	}
-
-	b, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return "", fmt.Errorf("invalid status code: %d %s", resp.StatusCode, string(b))
-	}
-
-	return string(b), nil
 }

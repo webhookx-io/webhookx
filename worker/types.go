10a95f1cabaa52d9f5d855c030c2d3d97ec05e31
Douglas-Lee <leeys.top@gmail.com>
2025-06-06 23:22:25 +0800

    perf: batch insert attemptDetail

--- worker/types.go
+++
@@ -1,8 +0,0 @@
-package worker
-
-type MessageData struct {
-	EventID    string `json:"event_id"`
-	EndpointId string `json:"endpoint_id"`
-	Attempt    int    `json:"attempt"`
-	Event      string `json:"event"`
-}

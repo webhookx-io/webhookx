49f31f183ded90c7fc5e3d15b5f58156e4005e2f
Douglas-Lee <leeys.top@gmail.com>
2026-02-05 20:56:38 +0800

    perf(serializer): use encoder/decoder pool to reduce overhead

--- pkg/serializer/msgpack.go
+++ pkg/serializer/msgpack.go
@@ -11,18 +11,27 @@ var MsgPack MsgPackSerializer
 type MsgPackSerializer struct{}
 
 func (s MsgPackSerializer) Serialize(val interface{}) ([]byte, error) {
-	var buffer bytes.Buffer
-	encoder := msgpack.NewEncoder(&buffer)
+	var buf bytes.Buffer
+
+	encoder := msgpack.GetEncoder()
+	defer msgpack.PutEncoder(encoder)
+
 	encoder.SetCustomStructTag("json")
+	encoder.Reset(&buf)
+
 	err := encoder.Encode(val)
 	if err != nil {
 		return nil, err
 	}
-	return buffer.Bytes(), nil
+	return buf.Bytes(), nil
 }
 
 func (s MsgPackSerializer) Deserialize(b []byte, val interface{}) error {
-	decoder := msgpack.NewDecoder(bytes.NewReader(b))
+	decoder := msgpack.GetDecoder()
+	defer msgpack.PutDecoder(decoder)
+
 	decoder.SetCustomStructTag("json")
+	decoder.Reset(bytes.NewReader(b))
+
 	return decoder.Decode(val)
 }
